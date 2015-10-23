f8cd8f675f1fa05397296551f021026818e7b2c1
Haipeng <njucs_wuhaipeng@163.com>
2015-10-23 15:52:11 +0800

    Add http wrapper

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # vchain
+
+## http sample
+
+    import "github.com/wuhp/vchain"
+
+    ...
+
+    vchain.SetOutput("/tmp/sample.vlog")
+
+    ...
+
+    func CreateUser(w http.ResponseWriter, r *http.Request) {
+        vr := NewRequestFromHttp(r, "WebServer", "PostUser")
+
+        ...
+
+        // invoke account service
+        client := &http.Client{}
+        req, _ := http.NewRequest(...)
+        WrapHttpRequest(req, NewChainHeader(vr.Uuid, true))
+        res, _ := client.Do(req)
+
+        ...
+
+        vr.EndWithCommit()
+    }
--- request.go
+++ request.go
@@ -2,6 +2,9 @@ package vchain
 
 import (
     "time"
+    "fmt"
+    "strconv"
+    "net/http"
     "encoding/json"
 
     "github.com/satori/go.uuid"
@@ -17,15 +20,20 @@ type Request struct {
     EndTs      int64  `json:"end_ts"`
 }
 
-func NewRequest(puuid, service, category string) *Request {
+func NewRequest(ch *ChainHeader, service, category string) *Request {
     r := new(Request)
     r.Uuid = uuid.NewV4().String()
-    r.ParentUuid = puuid
     r.Service = service
     r.Category = category
-    r.Sync = true
     r.BeginTs = time.Now().UTC().UnixNano()
-    
+    if ch == nil {
+        r.ParentUuid = ""
+        r.Sync = true
+    } else {
+        r.ParentUuid = ch.Uuid
+        r.Sync = ch.Sync
+    }
+
     return r
 }
 
@@ -41,4 +49,45 @@ func (r *Request) EndWithCommit() {
 func (r *Request) Commit() {
     message, _ := json.Marshal(r)
     print("request", string(message))
+}
+
+// Below two methods give a simple solution for http solution
+func NewRequestFromHttp(req *http.Request, service, category string) *Request {
+    ch := fetchChainHeader(req)
+    return NewRequest(ch, service, category)
+}
+
+func WrapHttpRequest(req *http.Request, ch *ChainHeader) {
+    if ch != nil {
+        req.Header.Set("Vchain-Uuid", ch.Uuid)
+        req.Header.Set("Vchain-Sync", fmt.Sprintf("%v", ch.Sync))
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+type ChainHeader struct {
+    Uuid string
+    Sync bool
+}
+
+func fetchChainHeader(req *http.Request) *ChainHeader {
+    uuid := req.Header.Get("Vchain-Uuid")
+    sync := req.Header.Get("Vchain-Sync")
+    if uuid == "" || sync == "" {
+        return nil
+    }
+
+    ch := new(ChainHeader)
+    ch.Uuid = uuid
+    ch.Sync, _ = strconv.ParseBool(sync)
+
+    return ch
+}
+
+func NewChainHeader(uuid string, sync bool) *ChainHeader {
+    ch := new(ChainHeader)
+    ch.Uuid = uuid
+    ch.Sync = sync
+    return ch
 }
